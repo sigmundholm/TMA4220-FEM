8fc9b48950f478428813fa53b49cced41be3821b
Sigmund Eggen Holm <sigmund.holm@gmail.com>
2020-10-08 19:15:50 +0200

    Implement and test Gauss quadrature in 1D, in one and two space dimension.

--- fem/quadrature_lib.py
+++ fem/quadrature_lib.py
@@ -2,6 +2,18 @@ import numpy as np
 
 from fem.quadrature import quadrature2D
 
+# This is quadrature points and weights for Gaussian quadrature in 1D.
+ONE_DIM_GAUSS = [[[0], [2]],  # 1-point rule
+                 [[-np.sqrt(1 / 3), np.sqrt(1 / 3)], [1, 1]],  # 2-point
+                 [[-np.sqrt(3 / 5), 0, np.sqrt(3 / 5)],  # 3-point
+                  [5 / 9, 8 / 9, 5 / 9]],
+                 [[-np.sqrt((3 + 2 * np.sqrt(6 / 5)) / 7),  # 4-point
+                   -np.sqrt((3 - 2 * np.sqrt(6 / 5)) / 7),
+                   np.sqrt((3 - 2 * np.sqrt(6 / 5)) / 7),
+                   np.sqrt((3 + 2 * np.sqrt(6 / 5)) / 7)],
+                  [(18 - np.sqrt(30)) / 36, (18 + np.sqrt(30)) / 36,
+                   (18 + np.sqrt(30)) / 36, (18 - np.sqrt(30)) / 36]]]
+
 # For Gaussian quadrature in barycentric coordinates. On the form
 #   (zeta-coordinates, weights)
 ONE_POINT = (np.array([[1 / 3, 1 / 3, 1 / 3]]), np.array([1]))
@@ -15,18 +27,45 @@ FOUR_POINTS = (np.array([[1 / 3, 1 / 3, 1 / 3], [3 / 5, 1 / 5, 1 / 5],
 class QGauss:
     """
     Used by FEValues for doing the numerical integration.
+
+    This class integrates can integrate over a
+     - cell
+         - triangle in 2D (then the points in the list domain_corners are
+           in R^2, and are the corners of the triangle). Then dim=2.
+         - line segment on real axis in 1D (then the points in domain_corners
+           are in R^1, and are the endpoints of the line segment). Then dim=1.
+     - boundary
+         - edge of a triangle in 2D (then the points triangle_corners are in
+           R^2, and are the endpoints of the line segment, which now lies in
+           the plane). Then dim=1.
+         - edge of a line segment in 1D, so just a single point. Then dim=0.
+
+    So the dim variable refers to the dimension of the integral, dim=1 of
+    along a line, dim=2 for an area etc. We should therefore have dim + 1
+    points in the domain_corners list, while the dimension of the points
+    could be anything (this is referred to as the space dimension, space_dim).
     """
     zetas = None
     weights = None
 
-    triangle_corners = None
+    # A list of points (a list, event when the point is a value in one
+    # dimension), describing the domain as the convex hull of these points.
+    domain_corners = []
     jacobi_determinant = 0
 
     def __init__(self, dim, degree):
         self.dim = dim
         self.degree = degree
 
-        if dim == 2:
+        if dim == 1:
+            if self.degree > len(ONE_DIM_GAUSS):
+                raise NotImplementedError("Need more quadrature points and "
+                                          "weights for 1D gauss of degree " +
+                                          str(degree))
+            self.points, self.weights = ONE_DIM_GAUSS[self.degree - 1]
+
+        elif dim == 2:
+            # The gauss quadrature points are given in barycentric coordinates.
             if degree == 1:
                 self.zetas, self.weights = ONE_POINT
             elif degree <= 3:
@@ -40,47 +79,78 @@ class QGauss:
             raise NotImplementedError()
 
     def reinit(self, points):
-        self.triangle_corners = points
-        a, d = points[1] - points[0]
-        b, e = points[2] - points[0]
-        # Jacobi determinant times 0.5 just because
-        magic = 0.5
-        self.jacobi_determinant = np.abs(a * e - b * d) * magic
+        if self.dim != len(points) - 1:
+            raise ValueError("The number of points constituting the domain "
+                             "is not corresponding to the dimension.")
+        self.domain_corners = points
+        space_dim = len(self.domain_corners[0])
 
-    def quadrature_point(self, q_index: int):
+        if self.dim == 1:
+            # The jacobi determinant in 1D. We just do a change of variable
+            # from [-1, 1] to the x-variable (first variable) in R^space_dim.
+            # NB: this will always calculate the length of the line if we
+            # integrate a constant of value 1. The line segment (b, a) for b
+            # > a, will also give the value |a - b|.
+            jacobi = 0.5 * np.abs(points[1][0] - points[0][0])
+            if space_dim == 1:
+                # When we just integrate along some interval, we only need
+                # the jacobi determinant to transform the integral from [-1,
+                # 1] to [a, b].
+                ds = 1
+            elif space_dim == 2:
+                # In two space dimensions, we integrate over the line ds,
+                # so we need a new change of variable from ds to dx. This is
+                # just multiplied together with the jacobi determinant.
+                delta_x, delta_y = points[1] - points[0]
+                line_inc = delta_y / delta_x
+                ds = np.sqrt(1 + line_inc ** 2)
+            else:
+                raise NotImplementedError()
+
+            self.jacobi_determinant = jacobi * ds
+
+        if self.dim == 2:
+            a, d = points[1] - points[0]
+            b, e = points[2] - points[0]
+            # Jacobi determinant times 0.5 just because
+            magic = 0.5
+            self.jacobi_determinant = np.abs(a * e - b * d) * magic
+
+    def quadrature_point(self, q_index: int) -> np.ndarray:
         """
         Return quadrature point with index q_index in the global coordinate
         system.
 
-        :param points: the vertices of the triangle/cell.
-        :param q_index:
-        :return:
+        :param q_index: index of the quadrature point.
+        :return: a vector of dimension dim.
         """
+        if self.dim == 0:
+            # TODO Integrate over just a single point, so no gauss (just for
+            # solving 1D PDE problems).
+            return self.domain_corners[0]
+
+        elif self.dim == 1:
+            # Transform from the quadrature coordinates on the interval [-1, 1]
+            # to points in R^dim.
+            assert len(self.domain_corners) == 2
+            a = self.domain_corners[0]
+            b = self.domain_corners[1]
+            q = self.points[q_index]
+            return 0.5 * ((b - a) * q + a + b)
+
         if self.dim == 2:
-            return self.triangle_corners.T @ self.zetas[q_index]
+            return self.domain_corners.T @ self.zetas[q_index]
         else:
             raise NotImplementedError()
 
     def quadrature(self, points: np.ndarray, g: callable):
-        if self.dim == 2:
+        if self.dim == 1:
+            pass
+
+        elif self.dim == 2:
             return quadrature2D(*points, Nq=self.degree, g=g)
         else:
             raise NotImplementedError()
 
     def jacobian(self):
         return self.jacobi_determinant
-
-
-if __name__ == '__main__':
-    # Verify with example from 1a)
-    pass
-    p1 = np.array([1, 0])
-    p2 = np.array([3, 1])
-    p3 = np.array([3, 2])
-
-    ps = np.array([p1, p2, p3])
-    print(ps)
-
-    q_gauss = QGauss(2, 1)
-    print()
-    print(q_gauss.quadrature_point(ps, 0))
--- tests/test_qgauss.py
+++ tests/test_qgauss.py
@@ -1,8 +1,9 @@
 import unittest
 
 import numpy as np
+import scipy.integrate
 
-from fem.quadrature_lib import QGauss
+from fem.quadrature_lib import QGauss, ONE_DIM_GAUSS
 
 
 class QGaussTest(unittest.TestCase):
@@ -53,3 +54,79 @@ class QGaussTest(unittest.TestCase):
                                        msg="Area of triangle not calculated "
                                            "correctly. Failed on these "
                                            "points: " + str(points))
+
+    def test_quadrature_const_func_one_dim_one_space_dim(self):
+        """ Test calculating the length of a line in R^1. """
+        func = 1
+
+        for degree in range(1, len(ONE_DIM_GAUSS) + 1):
+            # Check 10 random point setups for each quadrature degree.
+            for points in np.random.random(2 * 10).reshape(-1, 2, 1):
+                points *= 10
+
+                gauss = QGauss(dim=1, degree=degree)
+                gauss.reinit(points)
+
+                value = 0
+                for q_index in range(degree):
+                    value += func * gauss.weights[q_index] * gauss.jacobian()
+                exact_value = np.abs(points[1][0] - points[0][0])
+
+                self.assertAlmostEqual(value, exact_value, places=10,
+                                       msg="Not integrating a constant "
+                                           "correctly in one space dim. "
+                                           "Points = " + str(points))
+
+    def test_quadrature_const_func_one_dim_two_space_dims(self):
+        """ Test calculating the length of a line in R^2. """
+        func = 1
+
+        for degree in range(1, len(ONE_DIM_GAUSS) + 1):
+            # Check 10 random point setups for each quadrature degree.
+            for points in np.random.random(4 * 10).reshape(-1, 2, 2):
+                points *= 10
+                gauss = QGauss(dim=1, degree=degree)
+                gauss.reinit(points)
+
+                value = 0
+                for q_index in range(degree):
+                    value += func * gauss.weights[q_index] * gauss.jacobian()
+                delta_x, delta_y = points[1] - points[0]
+                exact_value = np.sqrt(delta_x ** 2 + delta_y ** 2)
+
+                self.assertAlmostEqual(value, exact_value, places=10,
+                                       msg="Not integrating a constant "
+                                           "correctly in one space dim. "
+                                           "Points = " + str(points))
+
+    def test_quadrature_polynomial_one_dim_one_space_dim(self):
+        """ Check degree of exacness for Gauss quadrature along a line in R^1"""
+        poly1 = lambda x: 3 * x + 4
+        poly2 = lambda x: x ** 2 + 5 * x - 7
+        poly3 = lambda x: 4 * x ** 3 - 8 * x ** 2 - 3 * x + 1
+        poly4 = lambda x: x ** 4 - 4 * x ** 3 - 8 * x ** 2 - 3 * x + 1
+        poly5 = lambda x: x ** 5 - 4 * x ** 3 - 8 * x ** 2 - 3 * x + 1
+
+        for func, poly_deg in [(poly1, 1), (poly2, 2), (poly3, 3),
+                               (poly4, 4), (poly5, 5)]:
+            for degree in range(1, len(ONE_DIM_GAUSS) + 1):
+                for points in np.random.random(2 * 5).reshape(-1, 2, 1):
+                    points *= 10
+
+                    gauss = QGauss(dim=1, degree=degree)
+                    gauss.reinit(points)
+
+                    value = 0
+                    for q_index in range(degree):
+                        x_q = gauss.quadrature_point(q_index)
+                        value += func(x_q[0]) * gauss.weights[q_index] \
+                                 * gauss.jacobian()
+                    exact_value = scipy.integrate.quad(func, a=min(points),
+                                                       b=max(points))[0]
+
+                    if poly_deg <= 2 * degree - 1:
+                        self.assertAlmostEqual(
+                            value, exact_value, places=10,
+                            msg=f"Expect {degree} point Gauss to integrate "
+                                f"{poly_deg}. degree polynom exactly."
+                                "Points = " + str(points))
