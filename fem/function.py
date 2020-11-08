d965c1d6f22712497082c7363dfd85b286100b96
Sigmund Eggen Holm <sigmund.holm@gmail.com>
2020-11-08 14:42:05 +0100

    Easy plotting of functions.
    A bit ugly though..

--- fem/function.py
+++ fem/function.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from fem.plotting import plot_solution
+
 
 class Function:
 
@@ -27,3 +29,15 @@ class Function:
         for p in points:
             values.append(self.value(p))
         return np.array(values)
+
+    def plot(self, points, triangles, plot_func=None):
+        values = self.value_list(points)
+        dim = len(values[0])
+
+        for d in range(dim):
+            func_values = values[:, d]
+            if plot_func is None:
+                ax = plot_solution(points, func_values, triangles, latex=True)
+            else:
+                ax = plot_func(points, func_values, triangles)
+            ax.set_title("Component " + str(d + 1))
